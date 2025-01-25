cd4f9de8e5318f8a1305f919a7d6cc66b267f737
pepela <pfuntusha@gmail.com>
2025-01-25 15:59:15 +0400

    first commit

---
+++ Scripts/jen.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+
+var player = null
+var state_machine
+
+const SPEED = 4.0
+const ATTACK_RANGE = 2.5
+
+@export var player_path := "Player"
+
+@onready var nav_agent = $NavigationAgent3D
+@onready var anim_tree = $AnimationTree
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_node(player_path)
+	state_machine = anim_tree.get("parameters/playback")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = Vector3.ZERO
+	
+	match state_machine.get_current_node():
+		"run":
+			nav_agent.set_target_position(player.global_transform.origin)
+			var next_nav_point = nav_agent.get_next_path_position()
+			velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+			rotation.y = lerp_angle(rotation.y, atan2(-velocity.x, -velocity.z), delta * 10.0)
+		"attack":
+			look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	
+	anim_tree.set("parameters/conditions/attack", _target_in_range())
+	anim_tree.set("parameters/conditions/run", !_target_in_range())
+	
+	anim_tree.get("parameters/playback")
+	
+	move_and_slide()
+
+func _target_in_range():
+	return global_position.distance_to(player.global_position) < ATTACK_RANGE
+
+func _hit_finished():
+	player._hit()
